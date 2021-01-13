d5afd3e169a52852e882a22dac42305219dc2dfe
Max Kadel <max@curationexperts.com>
2021-01-13 12:46:23 -0500

    Revert "Added dynatrace support & removed docker-compose (#15)"
    This reverts commit ae3104bab9809bf8392b00622b62299b5a167e03.

--- ops/boot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-if [ ! -z "$DYNATRACE_TOKEN" ];then
-  curl -Ls -H "Authorization: Api-Token ${DYNATRACE_TOKEN}" 'https://nhd42358.live.dynatrace.com/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default' > installer.sh
-
-  /bin/sh installer.sh --set-app-log-content-access=true --set-infra-only=true --set-host-group=DC --set-host-name=${CLUSTER_NAME}-iiif-image 2>&1 & 
-fi
-/bin/sh -c  'java -Dcantaloupe.config=/cantaloupe/cantaloupe.properties -jar /cantaloupe/cantaloupe-$CANTALOUPE_VERSION.war'
-
